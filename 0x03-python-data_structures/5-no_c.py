e8c52eac5f1b9cfeeaf6439e3523ba36e9851cc2
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-02 16:39:43 +0300

    Create new string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
         return
     length = len(my_string) - 1
     new_str = ""
-    return new_str.join([i for i in my_string if i != 'c' if i != 'C'])
+    return new_str.join([i for i in my_string if i != 'c' and i != 'C'])
