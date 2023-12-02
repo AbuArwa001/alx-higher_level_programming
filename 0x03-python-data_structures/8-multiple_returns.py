1b82aa46a96b5bef644b12fa6891a8c578f256e1
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-02 22:36:46 +0300

    Perorm multiple return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
 def multiple_returns(sentence):
-    if not sentence:
+    # if not sentence:
+    #     return 0, None
+    if len(sentence) == 0:
         return 0, None
     length = len(sentence)
     return length, sentence[0]
