6db52fac15e4312193a302aa9d192a8f56a78352
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-03 14:16:16 +0300

    Create Bool list for divisibility by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
+    if my_list is None:
+        return []
     return [i % 2 if i != 0 else True for i in my_list]
 #
 # def divisible_by_2(my_list):
