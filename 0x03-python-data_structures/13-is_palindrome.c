e6e513fbbe4fea075dbdb57c31f500324e49b978
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-03 17:16:06 +0300

    Improve performance

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -21,25 +21,26 @@ int is_palindrome(listint_t **head)
 		tail = tail->next;
 		size++;
 	}
-
 	arr = malloc(sizeof(int) * size);
 	tail = *head;
-
 	while (tail)
 	{
 		arr[i] = tail->n;
 		tail = tail->next;
 		i++;
 	}
-
 	len = size - 1;
-
 	while (h)
 	{
 		if (h->n != arr[len])
 		{
 			free(arr);
 			return (0);
+		}
+		if (len == size / 2 && h->n == arr[len])
+		{
+			free(arr);
+			return (1);
 		}
 		h = h->next;
 		len--;
