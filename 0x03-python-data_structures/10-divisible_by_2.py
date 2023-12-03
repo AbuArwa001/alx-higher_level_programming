35434455a922b6264b557a8c200cc0d9f40afca3
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-03 14:23:02 +0300

    Create Bool list for divisibility by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    boolist = my_list[:]
-    for count, i in enumerate(my_list):
-        if i % 2 == 0:
-            boolist[count] = True
-        else:
-            boolist[count] = False
-    return boolist
+    return [i % 2 == 0 for i in my_list]
+
+    # boolist = my_list[:]
+    # for count, i in enumerate(my_list):
+    #     if i % 2 == 0:
+    #         boolist[count] = True
+    #     else:
+    #         boolist[count] = False
+    # return boolist
 # def divisible_by_2(my_list=[]):
 #     if my_list is None:
 #         return []
