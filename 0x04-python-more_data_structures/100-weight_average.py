6804efc2f04314204610714af838159fd74c6317
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-07 01:32:27 +0300

    Weighted avarage

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    avg = 0
+    ttl = 0
+    for i in my_list:
+        avg += i[0] * i[1]
+        ttl += i[1]
+    return avg/ttl
+
+
+# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
