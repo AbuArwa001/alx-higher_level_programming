d20eaedd1586c5bb1d692cbecfcfc9f79518f5b6
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-18 21:33:36 +0300

    Safe print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    item_index = 0
+    try:
+        for index, item in enumerate(my_list):
+            item_index += 1
+            print(item, end="")
+            if x == item_index:
+                break
+        print()
+    except IndexError:
+        print(end="")
+    finally:
+        return item_index
+
