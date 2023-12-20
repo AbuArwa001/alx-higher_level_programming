1a5a2e21525b756508674d53c0cc5d1ce7a41f46
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-20 03:48:25 +0300

    Try squre

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,21 +1,3 @@
 #!/usr/bin/python3
 class Square:
-    """
-    This is an empty class representing a square.
-
-    Attributes:
-        None
-
-    Methods:
-        None
-
-    Example:
-        >>> square_instance = Square()
-        >>> square_instance
-        <__main__.Square object at 0x...>
-
-    Note:
-        This class is intentionally empty and serves as a starting point for
-        defining a square with attributes and methods.
-    """
     pass
