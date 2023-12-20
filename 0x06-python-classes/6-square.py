376a8f435cd4bf3f00be03afe0a340fc162a032d
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-20 05:53:19 +0300

    Add condition position > 0

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -20,6 +20,7 @@ class Square:
 
     @size.setter
     def size(self, value):
+        """"set size"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -61,7 +62,8 @@ class Square:
             print()
             return
         else:
-            [print("") for i in range(0, self.__position[1])]
+            if self.__position[1] > 0:
+                [print("") for i in range(0, self.__position[1])]
             for i in range(0, self.__size):
                 [print(" ", end="") for j in range(0, self.__position[0])]
                 [print("#", end="") for k in range(0, self.__size)]
