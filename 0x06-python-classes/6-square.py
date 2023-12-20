7469e49857b3a2273ec127982251c6ac1be1aa8b
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-20 05:55:35 +0300

    Add condition position > 0

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -65,6 +65,7 @@ class Square:
             if self.__position[1] > 0:
                 [print("") for i in range(0, self.__position[1])]
             for i in range(0, self.__size):
-                [print(" ", end="") for j in range(0, self.__position[0])]
+                if self.__position[1] > 0:
+                    [print(" ", end="") for j in range(0, self.__position[0])]
                 [print("#", end="") for k in range(0, self.__size)]
                 print("")
