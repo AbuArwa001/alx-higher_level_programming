9bb38bedffe5ab905592c3fad37e515671f2ea5d
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-20 04:08:11 +0300

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""THIS MODULE CREATES A SQUARE"""
+
+
+class Square:
+    """This is a square class
+    Args:
+        size: size of the square
+    Attributes:
+        size: size of the square
+    """
+    def __init__(self, size):
+        """Initialization of instance varibles"""
+        self.__size = size
