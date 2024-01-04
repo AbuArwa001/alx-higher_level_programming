990fe461aef39667431c22f26767130f1c835cc4
AbuArwa001 <khalfanathman12@yahoo.com>
2024-01-04 22:16:07 +0300

    Add comments to methodes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,15 +6,18 @@ class Rectangle:
     """Class rectangle"""
 
     def __init__(self, width=0, height=0):
+        """Atrributes initializations"""
         self._width = width
         self._height = height
 
     @property
     def width(self):
+        """Get the width of triangle"""
         return self._width
 
     @width.setter
     def width(self, value):
+        """Sets the width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -24,10 +27,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Gets the height"""
         return self._height
 
     @height.setter
     def height(self, value):
+        """Sets the height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
