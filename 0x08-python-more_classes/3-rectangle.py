b06a57670fb220d939563a3c70fe076e62a648ee
AbuArwa001 <khalfanathman12@yahoo.com>
2024-01-04 23:44:03 +0300

    Print class info using __str__

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -51,5 +51,7 @@ class Rectangle:
 
     def __str__(self):
         """Str string information for a class"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
         rows = ["#" * self.__width for _ in range(self.__height)]
         return "\n".join(rows)
