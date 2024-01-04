4d8401af899b41340252a8f3ee6fc4098893967b
AbuArwa001 <khalfanathman12@yahoo.com>
2024-01-04 22:23:37 +0300

    Insatiation set to public

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Atrributes initializations"""
-        self._width = width
-        self._height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
