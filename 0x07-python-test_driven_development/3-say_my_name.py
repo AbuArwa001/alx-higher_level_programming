eabfca7c313af5cead766ad4c7baee310b991551
AbuArwa001 <khalfanathman12@yahoo.com>
2024-01-07 20:41:47 +0300

    Modify for efficiancey

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,8 +8,25 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    Function for introduction.
+
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to an empty string.
+
+    Returns:
+        None
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print(f"My name is {first_name} {last_name}")
+
+    introduction = f"My name is {first_name} {last_name}" \
+        if first_name else f"My name is {last_name}"
+    print(introduction)
