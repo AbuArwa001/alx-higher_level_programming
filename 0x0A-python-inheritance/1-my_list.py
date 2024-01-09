33268bdb57e5430dde32df1a81fd68e04a2e9c6b
AbuArwa001 <khalfanathman12@yahoo.com>
2024-01-09 03:29:48 +0300

    Pycodestyle tes file 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,29 +1,8 @@
 #!/usr/bin/python3
-"""
-module that uses class MyLits
-"""
+"""class mylist"""
 
 
 class MyList(list):
-    """
-        class that inherits class List
-    """
-    def __init__(self):
-        """
-            init that initializes with parent class method
-        """
-        super().__init__()
-
+    """prints sorted list of class"""
     def print_sorted(self):
-        """
-        method that prints list
-        Returns:
-            None
-
-        """
-        if all(isinstance(element, int) for element in self):
-            sorted_list = sorted(self)
-            print(sorted_list)
-        else:
-            print("Unable to sort the list: "
-                  "All elements must be integers or floats.")
+        print(sorted(self))
