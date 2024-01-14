e6d3a9075a4f20011f5c72c6158fa718c8ddb4cf
AbuArwa001 <khalfanathman12@yahoo.com>
2024-01-14 20:04:21 +0300

    Add feature from obj to json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python3
+"""Base module for shapes"""
+import json
 
 
 class Base:
+    """Class for shapes base"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialization of base class"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of list_dictionaries
+        Args:
+            list_dictionaries:
+
+        Returns:
+            returns the JSON string representation of list_dictionaries
+        """
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
