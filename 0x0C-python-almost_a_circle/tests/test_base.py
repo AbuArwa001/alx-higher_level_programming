a9a79440630b7c98d4a0c587323799bf0a92978f
AbuArwa001 <khalfanathman12@yahoo.com>
2024-01-16 05:41:27 +0300

    Add tests

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python3
-"""Module for testing the Base class"""
-
-import unittest
-
+import os
 from models.base import Base
+import unittest
+from models.rectangle import Rectangle
 
 
 class TestBase(unittest.TestCase):
     """Test cases for the Base class"""
+
     def setUp(self):
-        # This method will be called before each test case
+        """This method will be called before each test case"""
         Base._Base__nb_objects = 0
 
     def test_instance_creation_with_id(self):
@@ -43,6 +42,50 @@ class TestBase(unittest.TestCase):
         b1 = Base()
         b2 = Base()
         self.assertEqual(type(b2)._Base__nb_objects, 2)
+
+    def test_to_json_string(self):
+        """Test the to_json_string method"""
+        r = Rectangle(1, 2, 3, 4)
+        json_string = Base.to_json_string([r.to_dictionary()])
+        self.assertEqual(json_string, '[{"id": 1, "width": 1, "height": 2, "x": 3, "y": 4}]')
+
+    def test_from_json_string(self):
+        """Test the from_json_string method"""
+        json_string = '[{"id": 1, "width": 1, "height": 2, "x": 3, "y": 4}]'
+        list_dicts = Base.from_json_string(json_string)
+        r = Rectangle(1, 2, 3, 4)
+        self.assertEqual(list_dicts, [r.to_dictionary()])
+
+    def test_create(self):
+        """Test the create method"""
+        r_dict = {'id': 1, 'width': 1, 'height': 2, 'x': 3, 'y': 4}
+        r = Rectangle.create(**r_dict)
+        self.assertIsInstance(r, Rectangle)
+        self.assertEqual(r.to_dictionary(), r_dict)
+
+    def test_save_to_file(self):
+        """Test the save_to_file method"""
+        r = Rectangle(1, 2, 3, 4)
+        Base.save_to_file([r])
+        with open('Rectangle.json', 'r', encoding='utf-8') as file:
+            content = file.read()
+            self.assertEqual(content, '[{"id": 1, "width": 1, "height": 2, "x": 3, "y": 4}]')
+
+    def test_load_from_file(self):
+        """Test the load_from_file method"""
+        r = Rectangle(1, 2, 3, 4)
+        Base.save_to_file([r])
+        list_objs = Base.load_from_file()
+        self.assertEqual(len(list_objs), 1)
+        self.assertIsInstance(list_objs[0], Rectangle)
+        self.assertEqual(list_objs[0].to_dictionary(), r.to_dictionary())
+
+    def tearDown(self):
+        """Clean up after each test case"""
+        try:
+            os.remove('Rectangle.json')
+        except FileNotFoundError:
+            pass
 
 
 if __name__ == '__main__':
