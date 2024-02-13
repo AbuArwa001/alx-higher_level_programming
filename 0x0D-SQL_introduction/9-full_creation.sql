c14778fb4390ecb0edbc70509edcdff43a395aea
AbuArwa001 <khalfanathman12@yahoo.com>
2024-02-13 23:29:34 +0300

    Create and insert into the table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- creates a table called second_table in the current database and set values
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO second_table (id,name,score) values(1,"john", 10);
+INSERT INTO second_table (id,name,score) values(2,"Alex", 3);
+INSERT INTO second_table (id,name,score) values(3,"Bob", 14);
+INSERT INTO second_table (id,name,score) values(4,"George", 8);
