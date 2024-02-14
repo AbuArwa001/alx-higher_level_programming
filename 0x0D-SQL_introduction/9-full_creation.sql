8577d4dab59707b380f03910e914d9b10f0c9370
AbuArwa001 <khalfanathman12@yahoo.com>
2024-02-14 06:00:25 +0300

    Add 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,6 @@
 -- creates a table called second_table in the current database and set values
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
-);
-INSERT IGNORE  INTO second_table (id,name,score) values(1,"john", 10);
-INSERT IGNORE  INTO second_table (id,name,score) values(2,"Alex", 3);
-INSERT IGNORE  INTO second_table (id,name,score) values(3,"Bob", 14);
-INSERT IGNORE  INTO second_table (id,name,score) values(4,"George", 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
