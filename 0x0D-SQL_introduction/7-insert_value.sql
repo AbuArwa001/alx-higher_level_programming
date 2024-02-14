ba27a33555605faddfd2838d22370d9f6731b840
AbuArwa001 <khalfanathman12@yahoo.com>
2024-02-14 05:33:34 +0300

    add semi colon

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,4 @@
 -- INSERT NEW ROW INTO THE first_table
-INSERT INTO first_table (id,name) values(89, "Best School")
+-- INSERT INTO second_table (id,name,score) values(5, "AZiz", 18);
+-- INSERT INTO second_table (id,name,score) values(6, "Aria", 12);
+INSERT INTO first_table (id,name) values(89, "Best School");
