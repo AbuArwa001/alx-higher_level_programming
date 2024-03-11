6cca3daf47203346c453c74eda704142a3d202d8
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-11 22:26:58 +0300

    Print first argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 let count = 0;
-let value = "";
-argv.forEach(element => {
-     if (count === 3) {
-    let value = element;
-    console.log(value);
-    }
+let value = '';
+argv.forEach((element) => {
+  value = element;
+  if (count >= 3) {
+    console.log(`${element}`);
+  }
   count++;
 });
-console.log(count, value);
 if (count === 2) {
   console.log('No argument');
 } else if (count === 3 && value) {
