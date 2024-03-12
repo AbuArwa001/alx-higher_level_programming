54bfd1abc505e6de8ef0f5d76512501f15e9ef5f
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-12 08:39:49 +0300

    Print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,6 +4,9 @@ if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < process.argv[2]; i++) {
-    console.log('X'.repeat(process.argv[2]));
+    for (let j = 0; j < process.argv[2]; j++) {
+      process.stdout.write('X');
+    }
+    console.log();
   }
 }
