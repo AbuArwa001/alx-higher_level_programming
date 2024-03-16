cc53fb12aa35b95c1b52968f3e63ecac7ea22441
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-16 04:47:50 +0300

    Add arument from termina

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,9 +2,15 @@
 """ Module for slecting rows in db
 """
 import MySQLdb
-db = MySQLdb.connect(user="root",
-                     passwd="Khalif01@2023",
-                     db="hbtn_0e_0_usa")
+import sys
+username = sys.argv[1]
+password = sys.argv[2]
+dbname = sys.argv[3]
+db = MySQLdb.connect(user=username,
+                     passwd=password,
+                     db=dbname,
+                     host="localhost",
+                     port=3306)
 cursor = db.cursor()
 cursor.execute("""SELECT *
 FROM states
