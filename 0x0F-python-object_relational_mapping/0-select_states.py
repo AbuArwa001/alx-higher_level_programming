d3ca8e3d5a07e32d8b8ae729d29ed86e9ce2e311
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-16 04:39:54 +0300

    Remove the two arguments

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,11 @@ import MySQLdb
 db = MySQLdb.connect(user="root",
                      passwd="Khalif01@2023",
                      db="hbtn_0e_0_usa")
-cursor=db.cursor()
-cursor.execute("""SELECT * 
+cursor = db.cursor()
+cursor.execute("""SELECT *
 FROM states
 ORDER BY states.id ASC""")
 
 if __name__ == "__main__":
     for i in cursor._rows:
         print(i)
-
