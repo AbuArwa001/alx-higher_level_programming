7746ec30db9b59ef4ee0f0722bd160456b765add
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-16 04:33:16 +0300

    Mysql connection

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Module for slecting rows in db
+"""
+import MySQLdb
+db = MySQLdb.connect(user="root",
+                     passwd="Khalif01@2023",
+                     db="hbtn_0e_0_usa",
+                     host="localhost",
+                     port=3306)
+cursor=db.cursor()
+cursor.execute("""SELECT * 
+FROM states
+ORDER BY states.id ASC""")
+for i in cursor._rows:
+    print(i)
+
