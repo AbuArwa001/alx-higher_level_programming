204054276799df0de0d1798792bca54e80373514
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-16 04:57:20 +0300

    Add arument from termina

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-""" Module for slecting rows in db
-"""
+""" Module for slecting rows in db """
 import MySQLdb
 import sys
-username = sys.argv[1]
-password = sys.argv[2]
-dbname = sys.argv[3]
-db = MySQLdb.connect(user=username,
-                     passwd=password,
-                     db=dbname,
-                     host="localhost",
-                     port=3306)
-cursor = db.cursor()
-cursor.execute("""SELECT *
-FROM states
-ORDER BY states.id ASC""")
-
-if __name__ == "__main__":
+def connector():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    db = MySQLdb.connect(user=username,
+                        passwd=password,
+                        db=dbname,
+                        host="localhost",
+                        port=3306)
+    cursor = db.cursor()
+    cursor.execute("""SELECT *
+    FROM states
+    ORDER BY states.id ASC""")
     for i in cursor._rows:
         print(i)
+    cursor.close()
+    db.close()
+if __name__ == "__main__":
+    connector()
