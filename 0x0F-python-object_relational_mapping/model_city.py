3c1b5ad3bd2caf1508c2c22bc1a0a2c6823ed299
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-17 14:11:48 +0300

    Model

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,8 +2,8 @@
 """ Module that contains the class definition of
 a State and an instance Base = declarative_base()"""
 from sqlalchemy import Column, Integer, String, Sequence , ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-
 Base = declarative_base()
 
 
@@ -15,4 +15,5 @@ class City(Base):
     id = Column(Integer, Sequence('states_id_seq'), primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'),  nullable=False)
+    cities = relationship("State",back_populates="cities")
 
