e203b7f833a83bd9529c4590020894cfbf250bab
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-18 02:52:07 +0300

    Pycode styling

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Module that contains the class definition of
 a State and an instance Base = declarative_base()"""
-from sqlalchemy import Column, Integer, String, Sequence , ForeignKey
+from sqlalchemy import Column, Integer, String, Sequence, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
+
 
 class City(Base):
     """
@@ -14,5 +14,3 @@ class City(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'),  nullable=False)
-    cities = relationship("State")
-
