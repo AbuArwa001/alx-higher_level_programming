0f708d8a9e54a72354832096782bf8f21dfc95ad
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-17 18:28:38 +0300

    Join tables with SqlAlchemy

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -11,7 +11,7 @@ class City(Base):
         Class for mapping States to states in Mysql DB
     """
     __tablename__ = 'cities'
-    id = Column(Integer, Sequence('states_id_seq'), primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'),  nullable=False)
     cities = relationship("State")
