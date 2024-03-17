7beb8ae78874dbae6bfbab5a52412dafa1358b7f
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-17 04:50:29 +0300

    Update state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Module that lists all State objects that contain the letter a from
+database hbtn_0e_4_usa Using SqlAlchemy"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def connector():
+    """
+    Retrieves and prints all rows from the 'states'
+    objects that contain the letter a from the database hbtn_0e_6_usa.
+
+    Requires three command line arguments:
+    - MySQL database username
+    - MySQL database password
+    - MySQL database name
+
+    Dependencies:
+    - Requires MySQLdb and SQLAlchemy libraries.
+
+    Example usage:
+    python script_name.py username password database_name
+
+    Assumes 'State' model mapping to 'states'
+    table is defined elsewhere with attributes 'id' and 'name'.
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    con_str = 'mysql+mysqldb://{}:{}@localhost/{}'.format(username,
+                                                          password,
+                                                          dbname)
+    engine = create_engine(con_str, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).filter(State.id == 2).first()
+    """     query = session.query(State.id, State.name).\
+    filter(State.id == 2).first() """
+
+    if query:
+        query.name = 'New Mexico'
+        session.commit()
+    else:
+        print("Not found")
+
+
+if __name__ == "__main__":
+    connector()
