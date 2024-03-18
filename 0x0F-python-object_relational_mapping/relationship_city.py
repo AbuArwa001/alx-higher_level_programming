2cd5e74fc3c506c30be87519205a39506f8c0ad6
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-18 03:36:39 +0300

    Relate city to state

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """ Module that contains the class definition of
 a State and an instance Base = declarative_base()"""
-from sqlalchemy import Column, Integer, String, Sequence, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from model_state import Base
+from relationship_state import Base
 
 
 class City(Base):
     """
-        Class for mapping States to states in Mysql DB
+    Class for mapping cities to City in Mysql DB
     """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'),  nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
     state = relationship("State", back_populates="cities")
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """ Module that contains the class definition of
 a State and an instance Base = declarative_base()"""
+# relationship_state.py
 from sqlalchemy import Column, Integer, String, Sequence
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
     """
-        Class for mapping States to states in Mysql DB
+    Class for mapping states to State in Mysql DB
     """
     __tablename__ = 'states'
     id = Column(Integer, Sequence('states_id_seq'), primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state")
+    cities = relationship("City", back_populates="state",
+                          cascade="all, delete, delete-orphan")
