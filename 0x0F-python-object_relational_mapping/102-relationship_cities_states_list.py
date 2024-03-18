81aef51e288cf55c614d8f209999d547295b42de
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-18 05:00:07 +0300

    Explore city - state relation

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" Module that lists all States from the
+database hbtn_0e_4_usa Using SqlAlchemy"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+
+
+from sqlalchemy import (create_engine)
+from sqlalchemy import label, select
+from sqlalchemy.orm import sessionmaker,  aliased
+from sqlalchemy.orm import joinedload
+
+
+def connector():
+    """
+    Retrieves and prints all rows from the 'states'
+    table in a MySQL database, ordered by 'id'.
+
+    Requires three command line arguments:
+    - MySQL database username
+    - MySQL database password
+    - MySQL database name
+
+    Dependencies:
+    - Requires MySQLdb and SQLAlchemy libraries.
+
+    Example usage:
+    python script_name.py username password database_name
+
+    Assumes 'State' model mapping to 'states'
+    table is defined elsewhere with attributes 'id' and 'name'.
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    con_str = 'mysql+mysqldb://{}:{}@localhost/{}'.format(username,
+                                                          password,
+                                                          dbname)
+    engine = create_engine(con_str, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+
+    stmt = session.query(State).subquery()
+    Q_object = session.query(City).options(joinedload(City.state))
+
+    for city in Q_object:
+        # <city id>: <city name> -> <state name>
+        # print(f"{state.cities.state}: {state.cities} -> {state.name}")
+        # print(f"{state.cities.state}: {state.cities} -> {state.name}")
+        print(f"{city.id}: {city.name} -> {city.state.name}")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    connector()
