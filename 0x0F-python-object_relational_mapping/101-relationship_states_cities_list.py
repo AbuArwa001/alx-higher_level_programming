b1b20de65bca4f5ec0c2c53034b4508e401d319a
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-18 04:09:57 +0300

    Join two relations and print

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+""" Module that lists all States from the
+database hbtn_0e_4_usa Using SqlAlchemy"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+
+
+from sqlalchemy import (create_engine)
+from sqlalchemy import label, select
+from sqlalchemy.orm import sessionmaker,  aliased
+
+
+def connector():
+    """
+    Retrieves and prints all rows from the 'states'
+    table in a MySQL database, ordered by 'id'.
+
+    Requires three command line arguments:
+    - MySQL database username
+    - MySQL database password
+    - MySQL database name
+
+    Dependencies:
+    - Requires MySQLdb and SQLAlchemy libraries.
+
+    Example usage:
+    python script_name.py username password database_name
+
+    Assumes 'State' model mapping to 'states'
+    table is defined elsewhere with attributes 'id' and 'name'.
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    con_str = 'mysql+mysqldb://{}:{}@localhost/{}'.format(username,
+                                                          password,
+                                                          dbname)
+    engine = create_engine(con_str, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+
+    stmt = session.query(City).subquery()
+    Q_object = session.query(State, stmt).\
+        join(stmt, State.id == stmt.c.state_id).order_by(stmt.c.id)
+    dup_list = []
+    print(Q_object)
+    for state,  city_id, city, st_id in Q_object:
+        if state.name not in dup_list:
+            print(f"{state.id}: {state.name}:")
+            for city in state.cities:
+                print(f"\t{city.id}: {city.name}")
+        dup_list.append(state.name)
+
+    session.close()
+
+
+if __name__ == "__main__":
+    connector()
