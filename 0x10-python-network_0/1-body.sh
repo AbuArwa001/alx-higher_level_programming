0f733ef3ff5acf45bbfe2c243358838cbbd8a1f1
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-28 02:21:57 +0300

    Refine command

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-status=$(curl -sI "$1" | grep "HTTP" | awk '{print $2}') && [ "$status" -eq 200 ] && curl "$1"
+status=$(curl -sI "$1" | grep "HTTP" | awk '{print $2}') && [ $status -eq 200 ] && curl $1
