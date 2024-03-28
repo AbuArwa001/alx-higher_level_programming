1b60edd1cc0e1216a68497d42c55bdeaaec04402
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-28 14:19:24 +0300

    Reformat file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,11 @@ from urllib import request
 url = 'https://alx-intranet.hbtn.io/status'
 req = request.Request(url)
 
-with request.urlopen(req) as response:
-    red = response.read()
-    print(f"Body response:")
-    print(f"\t- type: {type(red)}")
-    print(f"\t- content: {red}")
-    print(f"\t- utf8 content: {response.reason}")
+
+if __name__ == "__main__":
+    with request.urlopen(req) as response:
+        red = response.read()
+        print(f"Body response:")
+        print(f"\t- type: {type(red)}")
+        print(f"\t- content: {red}")
+        print(f"\t- utf8 content: {response.reason}")
