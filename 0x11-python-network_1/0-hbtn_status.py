4a91248d1039fbc4bf94b90cd820db5ac740b2e2
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-28 13:51:12 +0300

    Reformat file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,5 @@ with request.urlopen(req) as response:
     red = response.read()
     print(f"Body response:")
     print(f"\t- type: {type(red)}")
-    content_type = response.info().get('Content-Type')
-    if content_type and 'charset=utf-8' in content_type.lower():
-        print(f"\t- content: {red}")
+    print(f"\t- content: {red}")
     print(f"\t- utf8 content: {response.reason}")
