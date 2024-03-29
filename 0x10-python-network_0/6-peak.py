22bf797fe7bced4c1215a7cba01a1e6a77e917cd
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-30 02:50:03 +0300

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" function  to find_peak """
+""" Module  to find_peak """
 
 
 def find_peak(list_of_integers):
-
+    """ function  to find_peak """
     if list_of_integers is None or len(list_of_integers) == 0:
         return None
 
