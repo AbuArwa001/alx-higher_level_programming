49d38d680653266e498298bff0218e10cc708cc8
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-30 00:59:25 +0300

    Other version

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display only body of a 200 status code response #curl -sX GET $1 -L
-cmd=$(($(curl -sI "$1" | grep HTTP | awk '{print $2}'))) && [ "$cmd" -eq "200" ] && curl -s "$1" -L
+curl -s "$1" -L
