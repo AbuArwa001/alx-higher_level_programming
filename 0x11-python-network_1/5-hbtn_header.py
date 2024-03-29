3d9e627a390eec12fb46697d71b327736f5f2c91
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-29 04:28:00 +0300

    Print req id with requests package

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+
+Usage: ./4-hbtn_status.py
+"""
+
+if __name__ == "__main__":
+
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    request = requests.get(url)
+    print(request.headers['X-Request-Id'])
