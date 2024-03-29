c2f9c19469b84e73dac6c40096bc7c0a0c5fb198
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-29 04:41:30 +0300

    Post email with requests

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     email = sys.argv[2]
     values['email'] = email
     url = sys.argv[1]
-    response = requests.post(url, json=values)
+    response = requests.post(url, data=values)
     print(response.text)
