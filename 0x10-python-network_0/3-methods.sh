d9d7bd0b13281042be699052e127dfc89310dc63
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-30 01:56:34 +0300

    Shell check

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept
-curl -sIX  OPTIONS "$1" -L | grep "Allow" | awk '{print $1}'
+curl -sIX OPTIONS "$1" -L | grep "Allow" | awk -F: '{print $2}'
