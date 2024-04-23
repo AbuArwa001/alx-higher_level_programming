fc16f35af0a0b9c6d1a8418c0e75a6fc26743e5b
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-23 11:45:18 +0300

    Writes the content to a file s

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Writes the content to a file specified as the second command-line argument
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+function writeInFile (filePath, content) {
+  try {
+    fs.writeFileSync(filePath, content);
+    console.log(`Content written to ${filePath}`);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+writeInFile(filePath, content);
