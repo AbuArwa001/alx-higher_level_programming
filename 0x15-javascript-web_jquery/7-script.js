34e951e20fcd133024cf42a9b21ef4121fdee512
AbuArwa001 <khalfanathman12@yahoo.com>
2024-05-03 03:49:36 +0300

    Advanced

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,9 +1,9 @@
 const uri = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
-const $Div = $('div#character');
+const $divCharacter = $('div#character');
 
 $.ajax({
   url: uri,
   dataType: 'json'
 }).done((data) => {
-  $Div.text(data.name);
+  $divCharacter.text(data.name);
 });
